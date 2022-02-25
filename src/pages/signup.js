a6a44c6f9d78043b87d9598ac46e9001718915b0
Jacob Hein <jacobhein1892@gmail.com>
2022-02-24 21:09:47 -0800

    I have the signup logic and most of the error handeling working now

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -11,20 +11,27 @@ function Signup(){
         height_inches: 9
     }); 
 
+    const [displayError, setDisplayError] = useState(false); 
+
     function handleChange(event){
+        setDisplayError(false);
         let name = event.target.name;
         let value = event.target.value; 
         setNewUser(prev => ({...prev, [name]: value}))
     }
 
-    function handleSubmit(){
+    // function displayErrorText(){
+        
+    // }
+
+    function handleSubmit(event){
+        event.preventDefault(); 
         axios.put(`${process.env.REACT_APP_API}/users`, {newUser})
         .then((response) => {
             let data = response.data; 
-            if(data['error_empty'] || data['error_exists'] ){
-                document.getElementById('error').classList.toggle('show')
-                console.log("there's an error")
-                console.log(response.data); 
+            if(data['error_exists'] ){
+                console.log('error')
+                setDisplayError(true)
             } else {
                 console.log('No error')
                 console.log(response.data); 
@@ -44,13 +51,13 @@ function Signup(){
                 <h1 className='header_logo_alt'>Benchcoach</h1>
                     <form id='signup_form' onSubmit={handleSubmit}>
                         <label className='firstname'>First Name:</label>
-                        <input required name='first_name' className='firstname' onChange={handleChange}></input>
+                        <input required name='first_name' value={newUser.first_name} className='firstname' onChange={handleChange}></input>
                         <label className='lastname'>Last Name:</label>
-                        <input required name='last_name' className='lastname' onChange={handleChange}></input>
+                        <input required name='last_name' className='lastname' value={newUser.last_name} onChange={handleChange}></input>
                         <label className='email'>Email:</label>
-                        <input required type='email' name='email' className='email' onChange={handleChange}></input>
+                        <input required type='email' name='email' className='email' value={newUser.email} onChange={handleChange}></input>
                         <label className='password'>Password:</label>
-                        <input required type='password' name='password' className='password' onChange={handleChange}></input>
+                        <input required type='password' name='password' className='password' value={newUser.password} onChange={handleChange}></input>
                         <label className='throwing_hand'>Throwing Hand:</label>
                         <select name='throwing_hand' defaultValue={newUser.throwing_hand} className='throwing_hand' onChange={handleChange}>
                             <option value={'left'}>Left</option>
@@ -58,7 +65,7 @@ function Signup(){
                             <option value={'both'}>Both</option>
                         </select>
                         <label className='birth_year'>Birthday:</label>
-                        <input required type='date' name='birth_year' className='birth_year' onChange={handleChange}></input>
+                        <input required type='date' name='birth_year' className='birth_year' value={newUser.birth_year} onChange={handleChange}></input>
                         <label className='height'>Height:</label>
                         <div className='height'>
                             <div>
@@ -90,7 +97,9 @@ function Signup(){
                         </div>
                         <button className='button__signup' type='submit'>Sign Up</button>
                     </form>
-                
+                {displayError &&
+                    <p id='email_alert'>An account for this email already exists</p>
+                }
             </div>
         </div>
 
